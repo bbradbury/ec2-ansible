f1102fa4a4f736c6eaf31356066a6ff17d3ab762
Brent Bradbury <brent@brentium.com>
2015-01-19 05:56:17 -0600

    First commit

---
+++ export_ec2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#source export_ec2.sh credentials.csv
+
+CREDENTIALS=$(tail -1 $@)
+
+export AWS_ACCESS_KEY_ID=`echo $CREDENTIALS | cut -d',' -f2 | tr -d '\r'`
+export AWS_SECRET_ACCESS_KEY=`echo $CREDENTIALS | cut -d',' -f3 | tr -d '\r'`
