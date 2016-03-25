21eba9f55c21504249e2b227b294c3759a1d33dc
Brent Bradbury <brent@micasense.com>
2016-03-25 15:34:48 -0500

    Update for other apps that want other variables

--- export_ec2.sh
+++ export_ec2.sh
@@ -5,4 +5,6 @@
 CREDENTIALS=$(tail -1 $@)
 
 export AWS_ACCESS_KEY_ID=`echo $CREDENTIALS | cut -d',' -f2 | tr -d '\r'`
+export AWS_ACCESS_KEY=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=`echo $CREDENTIALS | cut -d',' -f3 | tr -d '\r'`
+export AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY
