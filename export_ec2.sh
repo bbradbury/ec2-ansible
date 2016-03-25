b61f670202d575606c23e52a19ea071f95307c46
Brent Bradbury <brent@micasense.com>
2016-03-25 15:36:59 -0500

    Update usage comment

--- export_ec2.sh
+++ export_ec2.sh
@@ -2,6 +2,10 @@
 
 #source export_ec2.sh credentials.csv
 
+#Actual usage in .bash_profile on os x:
+#source $HOME/ec2-ansible/export_ec2.sh $HOME/ec2/downloaded-credentials-file.csv`
+
+
 CREDENTIALS=$(tail -1 $@)
 
 export AWS_ACCESS_KEY_ID=`echo $CREDENTIALS | cut -d',' -f2 | tr -d '\r'`
